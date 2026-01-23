6164bbdbb2ad782a2b400013fc493583bf36a7d3
ruslon87 <79045975+ruslon87@users.noreply.github.com>
2026-01-24 01:40:59 +0500

    Add newline at end of main.dart

--- skidkz/lib/main.dart
+++ skidkz/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'package:skidkz/app.dart';
 import 'firebase_options.dart';
 
