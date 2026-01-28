9347ce909eeb741bc895b11baff14bbaeeb0173d
ruslon87 <79045975+ruslon87@users.noreply.github.com>
2026-01-29 00:51:31 +0500

    Create buyer_cart_screen.dart

---
+++ skidkz/lib/features/buyer/screens/buyer_cart_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BuyerCartScreen extends StatelessWidget {
+  const BuyerCartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Корзина (заглушка)'),
+    );
+  }
+}
