d9bd7750dfc9589d5cca77f1ca6d959a3278e2f4
ruslon87 <79045975+ruslon87@users.noreply.github.com>
2026-02-04 22:51:23 +0500

    Update product_repository.dart

--- skidkz/lib/data/product_repository.dart
+++ skidkz/lib/data/product_repository.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:skidkz/models/product.dart';
+import 'package:skidkz/data/models/product.dart';
 
 final productRepositoryProvider = Provider((ref) => ProductRepository(FirebaseFirestore.instance));
 
