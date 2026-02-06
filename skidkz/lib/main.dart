0627e90d31c03b1a992a539b42097a7caf64b500
ruslon87 <79045975+ruslon87@users.noreply.github.com>
2026-02-06 16:16:47 +0500

    Update main.dart

--- skidkz/lib/main.dart
+++ skidkz/lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+
+import 'app.dart';
 import 'firebase_options.dart';
-import 'features/home/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MaterialApp(home: HomePage()));
+  runApp(
+    const ProviderScope(
+      child: SkidKZApp(),
+    ),
+  );
 }
