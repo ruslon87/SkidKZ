5bde82c0073bd931319202c74d136cd9bde3b674
ruslon87 <79045975+ruslon87@users.noreply.github.com>
2026-02-09 23:46:27 +0500

    Update home_products_provider.dart

--- skidkz/lib/features/home/providers/home_products_provider.dart
+++ skidkz/lib/features/home/providers/home_products_provider.dart
@@ -1,14 +1,17 @@
+// lib/features/home/providers/home_products_provider.dart
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:skidkz/core/router/app_router.dart'; // firestoreProvider
-import 'package:skidkz/data/models/product.dart';
 import 'package:skidkz/data/product_repository.dart';
+import 'package:skidkz/data/models/product.dart';
 
+/// Репозиторий товаров (Firestore)
 final productRepositoryProvider = Provider<ProductRepository>((ref) {
-  final db = ref.watch(firestoreProvider);
-  return ProductRepository(db);
+  return ProductRepository(FirebaseFirestore.instance);
 });
 
+/// Активные (опубликованные) товары для витрины покупателя
 final activeProductsStreamProvider = StreamProvider<List<Product>>((ref) {
   final repo = ref.watch(productRepositoryProvider);
   return repo.watchActiveProducts();
