764b4d20938dd07ae7abc732eaa1a87ac04cc7b4
ruslon87 <79045975+ruslon87@users.noreply.github.com>
2026-02-27 00:38:09 +0500

    Update app.dart

--- skidkz/lib/app.dart
+++ skidkz/lib/app.dart
@@ -1,10 +1,11 @@
+// lib/app.dart
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import 'core/router/app_router.dart' as r;
 import 'core/theme/app_theme.dart';
-import 'core/widgets/app_back_handler.dart';
 
 class SkidKZApp extends ConsumerWidget {
   const SkidKZApp({super.key});
@@ -18,14 +19,6 @@ class SkidKZApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkTheme,
       routerConfig: router,
-
-      // ГЛОБАЛЬНЫЙ back-handler (один на всё приложение)
-      builder: (context, child) {
-        return AppBackHandler(
-          router: router,
-          child: child ?? const SizedBox.shrink(),
-        );
-      },
     );
   }
 }
